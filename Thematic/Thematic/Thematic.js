c93c9e7f576f843cfc30e7ae68b74d4ee986b63f
Niall <niall.odonnell@students.plymouth.ac.uk>
2019-05-31 22:12:31 +0100

    Thematic updated, use GetById instead of class

--- Thematic/Thematic/Thematic.js
+++ Thematic/Thematic/Thematic.js
@@ -4,6 +4,13 @@ var modal = document.getElementById("myModal");
 // Get the button that opens the modal
 var btn = document.getElementById("myBtn");
 
+var action = document.getElementById("action");
+
+var outcome = document.getElementById("outcome");
+
+var pusher = document.getElementById("pusher");
+
+var salsa = document.getElementById("salsa");
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
@@ -11,6 +18,8 @@ var span = document.getElementsByClassName("close")[0];
 btn.onclick = function() {
   modal.style.display = "block";
 }
+
+
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
@@ -23,3 +32,95 @@ window.onclick = function(event) {
     modal.style.display = "none";
   }
 }
+
+
+
+action.onclick = function ActionTheme() {
+
+    var header = document.getElementById("headerBar");
+    header.classList.toggle("main-action");
+
+    var accent1 = document.getElementById("accent1")
+    accent1.classList.toggle("accent-action");
+    var accent2 = document.getElementById("accent2")
+    accent2.classList.toggle("accent-action");
+    var accent3 = document.getElementById("accent3")
+    accent3.classList.toggle("accent-action");
+    var accent4 = document.getElementById("accent4")
+    accent4.classList.toggle("accent-action");
+
+
+    var text = document.getElementById("text")
+    text.classList.toggle("text-container-action");
+
+    modal.style.display = "none";
+}
+
+outcome.onclick = function OutcomeTheme() {
+
+    var header = document.getElementById("headerBar");
+    header.classList.toggle("main-outcome");
+    alert("yo");
+    var accent1 = document.getElementById("accent1")
+    accent1.classList.toggle("accent-outcome");
+    var accent2 = document.getElementById("accent2")
+    accent2.classList.toggle("accent-outcome");
+    var accent3 = document.getElementById("accent3")
+    accent3.classList.toggle("accent-outcome");
+    var accent4 = document.getElementById("accent4")
+    accent4.classList.toggle("accent-outcome");
+
+
+    var text = document.getElementById("text")
+    text.classList.toggle("text-container-outcome");
+
+    modal.style.display = "none";
+}
+
+pusher.onclick = function pusherTheme() {
+
+    var header = document.getElementById("headerBar");
+    header.classList.toggle("main-pusher");
+
+    var accent1 = document.getElementById("accent1")
+    accent1.classList.toggle("accent-pusher");
+    var accent2 = document.getElementById("accent2")
+    accent2.classList.toggle("accent-pusher");
+    var accent3 = document.getElementById("accent3")
+    accent3.classList.toggle("accent-pusher");
+    var accent4 = document.getElementById("accent4")
+    accent4.classList.toggle("accent-pusher");
+
+
+    var text = document.getElementById("text")
+    text.classList.toggle("text-container-pusher");
+
+    modal.style.display = "none";
+}
+
+salsa.onclick = function salsaTheme() {
+
+    var header = document.getElementById("headerBar");
+    header.classList.toggle("main-salsa");
+
+    var accent1 = document.getElementById("accent1")
+    accent1.classList.toggle("accent-salsa");
+    var accent2 = document.getElementById("accent2")
+    accent2.classList.toggle("accent-salsa");
+    var accent3 = document.getElementById("accent3")
+    accent3.classList.toggle("accent-salsa");
+    var accent4 = document.getElementById("accent4")
+    accent4.classList.toggle("accent-salsa");
+
+
+    var text = document.getElementById("text")
+    text.classList.toggle("text-container-salsa");
+
+    modal.style.display = "none";
+}
+
+/*$(document).ready(function () {
+    $('#action').click(function () {
+        $('main').toggleClass('main-outcome main-action');
+    });
+}) */
